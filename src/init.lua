0fd480375b7eaf684f6e61f8d9db386d59bdc0d0
David Minnerly <voxeldavid@gmail.com>
2019-09-08 17:32:31 -0400

    Hook up all the new functions to the public API

--- src/init.lua
+++ src/init.lua
@@ -1,16 +1,31 @@
 return {
+	audio = {
+		playRandomSound = require(script.audio.playRandomSound),
+	},
+
+	characters = {
+		clearAccessories = require(script.characters.clearAccessories),
+		combineDescriptions = require(script.characters.combineDescriptions),
+		isAirborne = require(script.characters.isAirborne),
+		isAlive = require(script.characters.isAlive),
+		isGrounded = require(script.characters.isGrounded),
+		loadAccessories = require(script.characters.loadAccessories),
+	},
+
 	colors = {
 		brighten = require(script.colors.brighten),
 		darken = require(script.colors.darken),
 	},
 
 	debug = {
-		pretty = require(script.debug.pretty)
+		pretty = require(script.debug.pretty),
+		visualizeRadius = require(script.debug.visualizeRadius),
 	},
 
 	generic = {
 		bind = require(script.generic.bind),
 		chooseRandom = require(script.generic.chooseRandom),
+		debounce = require(script.generic.debounce),
 		getSortedKeys = require(script.generic.getSortedKeys),
 		maybeCall = require(script.generic.maybeCall),
 		new = require(script.generic.new),
@@ -22,7 +37,8 @@ return {
 	},
 
 	networking = {
-		fireExceptTo = require(script.networking.fireExceptTo)
+		fireExceptTo = require(script.networking.fireExceptTo),
+		onNextEvent = require(script.networking.onNextEvent),
 	},
 
 	players = {
@@ -31,4 +47,9 @@ return {
 		getUserId = require(script.players.getUserId),
 		getUserIdFromCharacter = require(script.players.getUserIdFromCharacter),
 	},
+
+	timing = {
+		async = require(script.timing.async),
+		yield = require(script.timing.yield),
+	}
 }
